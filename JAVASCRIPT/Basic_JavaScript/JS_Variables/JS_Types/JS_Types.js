7e1e4cd36d7d75110ddc6b49909952a740007fef
Alek K <boltakziy@gmail.com>
2026-02-01 23:58:33 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/Basic_JavaScript/JS_Variables/JS_Types/JS_Types.js
@@ -0,0 +1,3 @@
+document.getElementById("kanin").innerHTML = "The type of '3' is " + typeof 3 + "<br>" + "." +
+											 "The type of '3.14' is " + typeof 3.14 + "<br>" + "." +
+											 "The type of 'En kanin og en and' is " + typeof "En kanin og en and" + ".";
