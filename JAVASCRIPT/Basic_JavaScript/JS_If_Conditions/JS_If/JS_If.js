df8da3bdeb7ed2e7a82e3515c39f54ada4b9e5d7
Alek K <boltakziy@gmail.com>
2026-02-11 22:34:16 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/Basic_JavaScript/JS_If_Conditions/JS_If/JS_If.js
@@ -0,0 +1,8 @@
+let fish = 3;
+let text = "Daniel has less fishes than Hanna.";
+
+if (fish > 7) {
+	text = "Daniel has more fishes than Hanna.";
+}
+
+document.getElementById("coelho").innerHTML = text;
