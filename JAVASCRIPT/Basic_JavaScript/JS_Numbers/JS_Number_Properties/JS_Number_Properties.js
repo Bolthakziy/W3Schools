47046c900e36d7fe7a666e85b2db31eb98590f79
Alek K <boltakziy@gmail.com>
2026-03-01 22:48:42 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/Basic_JavaScript/JS_Numbers/JS_Number_Properties/JS_Number_Properties.js
@@ -0,0 +1,5 @@
+let x = Number.MAX_VALUE;
+let y = Number.MIN_VALUE;
+let text = "In JAVASCRIPT the largest possible number is " + x + "<br>" + "In JAVASCRIPT the smallest possible number is " + y;
+
+document.getElementById("coelho").innerHTML = text;
