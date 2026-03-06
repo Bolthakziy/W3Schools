94acc721245632335155e99f854ef1b1cf7a61df
Alek K <boltakziy@gmail.com>
2026-03-06 23:50:28 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/Basic_JavaScript/JS_Functions/Function_Invocation/Function_Invocation.js
@@ -0,0 +1,12 @@
+function coelho()
+{
+	let alexandre = 1;
+	let ana = 2;
+	let daniel = 3;
+
+	return (alexandre + ana + daniel);
+}
+
+let text = coelho;
+
+document.getElementById("coelho").innerHTML = text;
