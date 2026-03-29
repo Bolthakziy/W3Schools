178bfd0e3351c1fc2acec1bc7003ed2366a5e65b
Alek K <boltakziy@gmail.com>
2026-03-29 22:57:26 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/JS_Arrays/JS_Array_Search/JS_Array_Search.js
@@ -0,0 +1,4 @@
+const EuEAmigos = ["Alexandre", "Ana", "Daniel"];
+let position = EuEAmigos.indexOf("Ana") + 1;
+
+document.getElementById("coelho").innerHTML = EuEAmigos + "<br>" + "Ana's position : " + position;
