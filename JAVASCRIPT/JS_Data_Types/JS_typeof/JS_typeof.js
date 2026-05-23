ff32dcaa43b9d373ab304aaf8b26495ab3864e6a
Alek K <boltakziy@gmail.com>
2026-05-23 22:41:03 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/JS_Data_Types/JS_typeof/JS_typeof.js
@@ -0,0 +1,6 @@
+document.getElementById("coelho").innerHTML = "'Alexnadre' is " + typeof("Alexnadre") + "<br><br>" +
+												"256 is " + typeof(256) + "<br><br>" +
+												"'true' is " + typeof(true) + "<br><br>" +
+												"123456n is " + typeof(123456n) + "<br><br>" +
+												"'Symbol()' is " + typeof(Symbol()) + "<br><br>" +
+												"'y' is " + typeof(y);
