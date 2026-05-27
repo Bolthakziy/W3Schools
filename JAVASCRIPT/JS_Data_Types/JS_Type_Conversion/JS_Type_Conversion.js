de6f9d8b523a61b40add2fdb33d28bca2ca3958a
Alek K <boltakziy@gmail.com>
2026-05-27 11:47:48 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/JS_Data_Types/JS_Type_Conversion/JS_Type_Conversion.js
@@ -0,0 +1,6 @@
+let PIValue = "3.14";
+let radius = 7;
+let text = "The circle's area : "
+let area = 7 * 7 * Number(PIValue);
+
+document.getElementById("coelho").innerHTML = text + area + " cm^2.";
