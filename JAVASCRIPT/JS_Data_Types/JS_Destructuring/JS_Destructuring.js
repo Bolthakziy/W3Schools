f8680d4406513a7dc3bacbd251ed72eeda8b19cf
Alek K <boltakziy@gmail.com>
2026-05-28 11:00:08 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/JS_Data_Types/JS_Destructuring/JS_Destructuring.js
@@ -0,0 +1,9 @@
+const eu = {
+	firstName:"Alexandre",
+	lastName:"Coelho",
+	age:17
+};
+
+let {firstName, lastName} = eu;
+
+document.getElementById("coelho").innerHTML = firstName + " " + lastName;
