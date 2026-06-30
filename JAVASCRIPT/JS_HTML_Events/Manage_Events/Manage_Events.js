0dd9261a905359b0fbf7f7cf140ae23949b18de4
Alek K <boltakziy@gmail.com>
2026-06-30 23:13:19 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/JS_HTML_Events/Manage_Events/Manage_Events.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+btn.addEventListener("click", myFunction);
+
+function myFunction() {
+	document.getElementById("coelho").innerHTML = "Eu sou o Alexandre.";
+}
