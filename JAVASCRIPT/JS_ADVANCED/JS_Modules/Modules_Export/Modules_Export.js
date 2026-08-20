66ab3cf3943cb3519167c9bde859280ad1c6c3b0
Alek K <boltakziy@gmail.com>
2026-08-21 07:01:49 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/JS_ADVANCED/JS_Modules/Modules_Export/Modules_Export.js
@@ -0,0 +1,4 @@
+import {name, age} from "./pessoa.js";
+
+let texto = "My name is " + name + " and i am" + age + " years old.";
+document.getElementById("coelho").innerHTML = texto;
---
+++ JAVASCRIPT/JS_ADVANCED/JS_Modules/Modules_Export/pessoa.js
@@ -0,0 +1,4 @@
+const name = "Alexandre";
+const age = 17;
+
+export {name, age};
