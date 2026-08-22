10d305228ab3845bce2b48621fd2a6d355bee74e
Alek K <boltakziy@gmail.com>
2026-08-22 23:45:18 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/JS_ADVANCED/JS_Modules/Modules_Import/Modules_Import.js
@@ -0,0 +1,6 @@
+import {add, square} from './matematica.js';
+
+let result1 = add(14, 21);
+let result2 = square(11);
+
+document.getElementById("coelho").innerHTML = "14 + 21 = " + result1 + "<br>" + "11 X 11 = " + result2;
---
+++ JAVASCRIPT/JS_ADVANCED/JS_Modules/Modules_Import/matematica.js
@@ -0,0 +1,9 @@
+export function add(x, y)
+{
+	return (x + y);
+}
+
+export function square(x)
+{
+	return (x * x);
+}
