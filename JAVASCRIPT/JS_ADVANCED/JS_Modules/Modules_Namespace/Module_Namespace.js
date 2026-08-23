f116f7e8b870e8d7a227f2839cf8bd13f257052f
Alek K <boltakziy@gmail.com>
2026-08-23 20:23:30 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/JS_ADVANCED/JS_Modules/Modules_Namespace/Module_Namespace.js
@@ -0,0 +1,6 @@
+import * as math from "./math_module.js";
+
+let result1 = math.add(3, 7);
+let result2 = math.multiply(3, 7);
+
+document.getElementById("coelho").innerHTML = "3 + 7 = " + result1 + "<br>" + "3 X 7 = " + result2;
