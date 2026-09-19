cf887365e4be233eeca0aa1cf6a425f07b236f41
Alek K <boltakziy@gmail.com>
2026-09-19 23:40:38 +0900

    JAVASCRIPT

---
+++ JAVASCRIPT/JS_ADVANCED/JS_Graphics/JS_Canvas/JS_Canvas.js
@@ -0,0 +1,33 @@
+const canvas = document.getElementByID("myCanvas");
+const ctx = canvas.getContext("2d");
+
+cts.fillStyle = "#FF0000";
+canvas.height = canvas.width;
+ctx.transform(1, 0, 0, -1, 0, canvas.height);
+
+let xMax = canvas.height;
+let yMax = canvas.width;
+let scope = 1.2;
+let intercept = 70;
+const xArray = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+const yArray = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+
+ctx.fillStyle = "red";
+
+for (let i = 0; i < xArray.length - 1; ++i) {
+	let x = xArray[i] * xMax / 150;
+	let y = yArray[i] * yMax / 15;
+
+	ctx.beginPath();
+	ctx.ellipse(x, y, 3, 3, 0, 0, Math.PI * 2);
+	ctx.fill();
+}
+
+ctx.moveTo(0, intercept);
+ctx.lineTo(xMax, f(xMax));
+ctx.strokeStyle = "black";
+ctx.stroke();
+
+function f(x) {
+	return (x * slope + intercept);
+}
